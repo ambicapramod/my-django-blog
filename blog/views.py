72b37df17e09fe7fbb5872d456b3b0ec4b8b1534
ambicapramod <ta.pramod143@aol.in>
2017-08-31 23:06:26 +0530

    ok

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import post
 from django.utils import timezone
 def post_list(request):
@@ -6,6 +6,9 @@ def post_list(request):
 
 	
 	return render(request,'blog/post_list.html',{'posts':posts})
+def post_detail(request,pk):
+	Post = get_object_or_404(post,pk=pk)
 
+	return render(request,'blog/post_detail.html',{'post':Post})
 
 
